0801d09da1b40712dc1264ecb980bf5cbf5a2988
Ray <iinoshirozheng@gmail.com>
2026-01-18 20:47:47 +0800

    feat: Add a collection of `ripgrep` aliases and a search function for enhanced file searching.

--- stow/zsh/.config/zsh/conf.d/aliases.zsh
+++ stow/zsh/.config/zsh/conf.d/aliases.zsh
@@ -50,3 +50,27 @@ alias gst='git status'
 alias gp='git push'
 alias gl='git pull'
 
+# -- Ripgrep Shortcuts --
+# 增強顏色: 行號黃色、路徑綠色粗體、匹配紅色粗體
+if command -v rg >/dev/null; then
+  _rg_color='--colors=line:fg:yellow --colors=path:fg:green --colors=path:style:bold --colors=match:fg:red --colors=match:style:bold'
+
+  # 基本搜尋 (支援 pipeline: echo "pattern" | rgg)
+  rgg() { 
+    if [[ -p /dev/stdin ]]; then
+      command rg ${=_rg_color} --pretty "$@"
+    else
+      command rg ${=_rg_color} --pretty "$@"
+    fi
+  }
+  
+  alias rgf='rg --files | rg --color=always' # 搜尋檔名
+  alias rgi="rg ${=_rg_color} -i"             # 不區分大小寫
+  alias rgh="rg ${=_rg_color} --hidden"       # 包含隱藏檔案
+  alias rga="rg ${=_rg_color} --hidden --no-ignore"  # 搜尋所有檔案
+  alias rgt="rg ${=_rg_color} -t"             # 搜尋指定類型 (e.g. rgt py "pattern")
+  alias rgtls='rg --type-list | rg --color=always'   # 列出所有類型
+  alias rgc="rg ${=_rg_color} -C 3"           # 顯示上下文 3 行
+  alias rge="rg ${=_rg_color} -g"             # 搜尋指定副檔名 (e.g. rge "*.tsx" "pattern")
+fi
+
